8539039a6f8c9229fa3de882496c0d61067cdb8b
问崖 <pengxingjian.pxj@alibaba-inc.com>
2015-05-28 18:31:57 +0800

    slugify

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var assign = require('object-assign');
 var filter = require('./lib/filter');
+var slugify = require('uslug');
 
-hexo.config.toc = assign({}, hexo.config.toc);
+hexo.config.toc = assign({
+    slugify: function (str, opts) {
+        return slugify(str, opts);
+    }
+}, hexo.config.toc);
 
 hexo.extend.filter.register('before_post_render', filter.insert);
 hexo.extend.filter.register('after_post_render', filter.heading);
