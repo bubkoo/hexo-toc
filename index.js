2de4fb9e6bf1448ba8d77f60c9d7a8a45dd63f60
问崖 <pengxingjian.pxj@alibaba-inc.com>
2015-05-28 17:33:40 +0800

    fix: heading

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var assign = require('object-assign');
+var filter = require('./lib/filter');
 
 hexo.config.toc = assign({}, hexo.config.toc);
 
-hexo.extend.filter.register('before_post_render', require('./lib/filter'));
+hexo.extend.filter.register('before_post_render', filter.insert);
+hexo.extend.filter.register('after_post_render', filter.heading);
